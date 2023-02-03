08e1193a2996e57c8de32399b9f7016922f1349f
Yihong Song <ys377@duke.edu>
2023-02-03 17:20:04 -0500

    Update Background

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import { useEffect, useState } from "react";
 export default function Home() {
+    const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+    useEffect(() => {
+        function updateMouse(e) {
+            e.preventDefault();
+            setMousePos({ x: e.clientX, y: e.clientY });
+            // console.log(mousePos);
+        }
+        // add event listener
+        window.addEventListener("mousemove", updateMouse);
+        moveSpotlight();
+        // remove event listener on cleanup
+        return () => window.removeEventListener("mousemove", updateMouse);
+    });
+
+    function moveSpotlight() {
+        // const spotlight = document.querySelector(".spotlight");
+        // spotlight.style.top = `${mousePos.y - 100}px`;
+        // spotlight.style.left = `${mousePos.x - 100}px`;
+    }
+
     return (
         <>
-            <div></div>
+            <div className="bg-[url('/temp.svg')] w-[100vw] h-[100vh] bg-center bg-cover"></div>
         </>
     );
 }
