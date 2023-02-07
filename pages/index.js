19b4ac6412e4db7a251a8a0bf427882e3f467ee0
Yihong Song <ys377@duke.edu>
2023-02-07 12:44:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react";
-export default function Home() {
-    const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-    useEffect(() => {
-        function updateMouse(e) {
-            e.preventDefault();
-            setMousePos({ x: e.clientX, y: e.clientY });
-            // console.log(mousePos);
-        }
-        // add event listener
-        window.addEventListener("mousemove", updateMouse);
-        moveSpotlight();
-        // remove event listener on cleanup
-        return () => window.removeEventListener("mousemove", updateMouse);
-    });
+import { useRouter } from "next/router";
 
-    function moveSpotlight() {
-        // const spotlight = document.querySelector(".spotlight");
-        // spotlight.style.top = `${mousePos.y - 100}px`;
-        // spotlight.style.left = `${mousePos.x - 100}px`;
-    }
+export default function Home() {
+    const router = useRouter();
 
     return (
         <>
-            {/* <div className="w-[100%] h-[3rem]">hi</div> */}
+            <div className="w-[100%] h-[5rem] flex items-center justify-center bg-[#121432] text-white">
+                <a
+                    className="w-[30%] h-[3rem] m-[5%] flex items-center justify-center  border-2 hover:cursor-pointer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.hackduke.org/">
+                    Our Website
+                </a>
+                <a
+                    className="w-[30%] h-[3rem] m-[5%] flex items-center justify-center border-2 hover:cursor-pointer"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="mailto:sponsorship@hackduke.org">
+                    Sponsor Us
+                </a>
+            </div>
             <div className="bg-[url('/temp_mobile.svg')] lg:bg-[url('/temp.svg')] w-[100vw] h-[100vh] lg:h-[120vh] bg-center bg-cover"></div>
         </>
     );
